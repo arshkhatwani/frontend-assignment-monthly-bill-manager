5b45d7ee30d03b868750db87ee6ecd15b3ffcca5
Arsh Khatwani <arsh.khatwani@gmail.com>
2022-10-29 12:18:31 +0530

    Add total billed amount

--- src/components/Bills/BillsBar.jsx
+++ src/components/Bills/BillsBar.jsx
@@ -1,9 +1,14 @@
 import { Typography } from "@mui/material";
 import React from "react";
+import { useSelector } from "react-redux";
 import AddNewBill from "./AddNewBill";
 import CategoryFilter from "./CategoryFilter";
 
 function BillsBar() {
+  const { bills } = useSelector((store) => store.bills);
+  let sum = 0;
+  bills.forEach((e) => (sum += e.amount));
+
   return (
     <>
       <div className="flex flex-row justify-around items-center py-2 mb-8">
@@ -11,8 +16,13 @@ function BillsBar() {
 
         <AddNewBill />
       </div>
-      <div>
+
+      <div className="flex flex-row justify-around items-center py-2 mb-8">
         <CategoryFilter />
+
+        <div>
+          Total amount: <b>{sum}</b>
+        </div>
       </div>
     </>
   );
